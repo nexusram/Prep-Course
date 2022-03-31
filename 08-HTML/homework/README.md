3ee70e9fc190a13dd12aec60b2b549ff34039cc1
nexusram <nahuelsuarez97@hotmail.com>
2022-03-30 21:14:43 -0300

    homework extra y homework html y css

--- 08-HTML/homework/README.md
+++ 08-HTML/homework/README.md
@@ -31,9 +31,9 @@ II Añade un poco de estilo
   E. Agregue una regla de estilo a su elemento h1 cambiando el color del texto (Listado de colores: https://www.crockford.com/wrrrld/color.html).
   F. Agregue una regla de estilo al elemento img dándole un ancho de 400px.
   G. Agregue una regla de estilo a 'thirdDiv' cambiando la altura a 600px y el ancho a 500px.
-  H. Agregue una regla de estilo a 'thirdDiv' cambiando el color de fondo por el que quiera.
-  I. Cambie el tamaño de la fuente para 'spanId' a 18px.
-  J. Asignar al id 'spanId' un margen de 50px.
+  H. Agregue una regla de estilo a 'thirdDiv' cambiando el color de fondo por el que quiera.//
+  I. Cambie el tamaño de la fuente para 'spanId' a 18px.//
+  J. Asignar al id 'spanId' un margen de 50px.//
   K. Asignar al id 'thirdDiv' un relleno (padding) de 50px.
   L. Asignar al id 'thirdDiv' un borde con cualquier ancho de color y estilo que elija.
 ```
--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,6 +1,6 @@
 // No cambies los nombres de las funciones.
 
-function deObjetoAmatriz(objeto){
+function deObjetoAmatriz(objeto) {
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
   //Ejemplo: 
@@ -10,6 +10,7 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+return retorno;
 }
 
 
@@ -18,6 +19,14 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  retorno = {};///objeto clave valor
+  for (i = 0; string.length > i; i++) {
+    if (!retorno[string[i]]) {
+      retorno[string[i]] = 0;
+    }
+    retorno[string[i]]+=1;
+  }
+  return retorno;
 }
 
 
@@ -26,6 +35,30 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  caracter = [];
+  aux = [];
+  for (i = 0; i < s.length; i++) {
+    if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+      caracter.push(s.charAt(i));
+    } else {
+      aux.push(s.charAt(i));
+    }
+  }
+  //caracter.reverse();
+  result_2 = union(caracter);
+  result_1 = union(aux);
+  result_2 = result_2 + result_1;
+
+  function union(valor) {
+    aux3 = valor.toString()
+    var result = "";
+
+    for (y = 0; aux3.length > y; y++) {
+      result = aux3.split(',').join('');
+    }
+    return result;
+  }
+  return result_2;
 }
 
 
@@ -35,21 +68,40 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var espejo = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    espejo += str[i];
+  };
+  return espejo.split(' ').reverse().join(' ');
+}
 
 
-function capicua(numero){
+function capicua(numero) {
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  if (numero.toString().split('').reverse().join('') === numero.toString()) {
+    return "Es capicua";
+  } else {
+    return "No es capicua";
+  }
 }
 
 
-function deleteAbc(cadena){
+function deleteAbc(cadena) {
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  retorno = '';
+  y = 0;
+  for (i = 0; i < cadena.length; i++) {
+    if (cadena.charAt(i) !== "a" && cadena.charAt(i) !== "b" && cadena.charAt(i) !== "c") {
+      retorno = retorno + cadena[i];
+      y++;
+    }
+  }
+  return retorno;
 }
 
 
@@ -57,30 +109,48 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  retorno=[];
+  aux='';
+  for (i = 0; i < arr.length; i++) {
+    aux=arr[i].toString().split(",");
+      for(y=0;y<arr.length;y++)
+      {
+       
+      }
+  }
+return retorno;
 }
 
 
-function buscoInterseccion(arreglo1, arreglo2){
+function buscoInterseccion(arreglo1, arreglo2) {
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  aux = [];
+  for (i = 0; arreglo1.length > i; i++) {
+    for (y = 0; arreglo2.length > y; y++) {
+      if (arreglo2[y] === arreglo1[i]) {
+        aux.push(arreglo2[y]);
+      }
+    }
+  }
+  return aux;
 }
-
 
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-   deObjetoAmatriz,
-   numberOfCharacters,
-   capToFront,
-   asAmirror,
-   capicua,
-   deleteAbc,
-   sortArray,
-   buscoInterseccion,
+  deObjetoAmatriz,
+  numberOfCharacters,
+  capToFront,
+  asAmirror,
+  capicua,
+  deleteAbc,
+  sortArray,
+  buscoInterseccion,
 };
 
